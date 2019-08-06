63688eb02863e8b3a91ecadbc024a4608c8fa572
liamcristello <liamcristello@gmail.com>
2019-08-06 16:45:19 -0400

    Basic Pong Functionality
    Ball moves back and forth, bounces off walls and paddles and carries its momentum.

---
+++ Assets/Scripts/BallTrajectory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTrajectory : MonoBehaviour
+{
+    public Vector3 initialImpulse;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(initialImpulse, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
