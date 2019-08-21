0a654d38ebb2732d239a4dede27f6edb997418dc
liamcristello <liamcristello@gmail.com>
2019-08-21 16:56:05 -0400

    Semi-functional boundaries, base of spawning
    Semi-functional boundaries limiting paddle to interior of game screen. Hard-coded, unfortunately, hoping to abstract. Spawn script begun but not finished.

--- Assets/Scripts/MovePaddle.cs
+++ Assets/Scripts/MovePaddle.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class MovePaddle : MonoBehaviour
 {
+    // Vector2 representing input from the Wii Balance Board
     Vector2 wiiBB;
+    // float to allow adjustment to the paddle's sensitivity
+    //public static float paddleSens;
+    // Gameobjects representing the walls of the game screen
+    public GameObject eastWall, westWall;
+    // Vector3s to represent the inner edges of the game screen walls
+    Vector3 eastEdge, westEdge;
+    // float to manage paddle's x-position and bind it within game screen
+    float paddleX;
     // Start is called before the first frame update
     void Start()
     {
-
+        eastEdge = eastWall.GetComponent<BoxCollider>().center - eastWall.GetComponent<BoxCollider>().size;
+        westEdge = westWall.GetComponent<BoxCollider>().center + westWall.GetComponent<BoxCollider>().size;
+        //Debug.Log("Bounds are " + westEdge + " and " + eastEdge + ".");
     }
 
     // Update is called once per frame
@@ -16,9 +27,12 @@ public class MovePaddle : MonoBehaviour
     {
         wiiBB = CoPtoCM(Wii.GetCenterOfBalance(0));
         Debug.Log(CoPtoCM(Wii.GetCenterOfBalance(0)));
-        transform.position = wiiBB;
+        //paddleX = Mathf.Clamp(wiiBB.x, transform.TransformPoint(westEdge).x, transform.TransformPoint(eastEdge).x);
+        paddleX = Mathf.Clamp(wiiBB.x, -15.1f, 15.06f);
+        transform.position = new Vector3(paddleX, wiiBB.y);
     }
 
+    // Method to return Wii Balance Board's balance point input as a Vector2
     public static Vector2 CoPtoCM(Vector2 posn)
     {
         //return new Vector2(posn.x * 43.3f / 2f, posn.y * 23.6f / 2f);
