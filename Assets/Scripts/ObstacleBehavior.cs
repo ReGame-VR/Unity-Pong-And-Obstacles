cb0ab8290118e3a07dfa862e95713ffc976fb808
liamcristello <liamcristello@gmail.com>
2019-09-03 16:57:02 -0400

    Better Pathing
    Obstacle tracks camera position and paths to it, does not destroy properly however. Will fix.

--- Assets/Scripts/ObstacleBehavior.cs
+++ Assets/Scripts/ObstacleBehavior.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class ObstacleBehavior : MonoBehaviour
 {
+    //public GameObject startingPoint;
+    GameObject camera;
+    public float speed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = GameObject.Find("Camera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y + Time.deltaTime, transform.position.z);
+        float step = speed * Time.deltaTime;
+        //transform.position = new Vector3(transform.position.x, transform.position.y + Time.deltaTime, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, camera.transform.position, step);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Controller"))
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
