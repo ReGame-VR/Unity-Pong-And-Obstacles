f2275a540dd33fc23dac841cff3fd5fac220d4f6
liamcristello <liamcristello@gmail.com>
2019-09-04 16:57:42 -0400

    More Obstacle Adjustments
    Can rotate freely and fly at player. Still working on proper unloading, however. Do not seem to interact with Camera collider for whatever reason.

--- Assets/Scripts/ObstacleBehavior.cs
+++ Assets/Scripts/ObstacleBehavior.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ObstacleBehavior : MonoBehaviour
 {
-    //public GameObject startingPoint;
+    
     GameObject camera;
     public float speed = 1.0f;
+    public float rotX = 0;
+    public float rotY = 0;
+    public float rotZ = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +22,22 @@ public class ObstacleBehavior : MonoBehaviour
         float step = speed * Time.deltaTime;
         //transform.position = new Vector3(transform.position.x, transform.position.y + Time.deltaTime, transform.position.z);
         transform.position = Vector3.MoveTowards(transform.position, camera.transform.position, step);
+        transform.Rotate(Vector3.right, rotX * Time.deltaTime);
+        transform.Rotate(Vector3.up, rotY * Time.deltaTime);
+        transform.Rotate(Vector3.forward, rotZ * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (!collision.gameObject.CompareTag("Controller"))
+        Debug.Log("Collided");
+        if (collision.gameObject.CompareTag("Controller"))
         {
-            Destroy(this.gameObject);
+            Debug.Log("Deflected obstacle!");
         }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Obstacle struck player...");
+        }
+        Destroy(this.gameObject);
     }
 }
