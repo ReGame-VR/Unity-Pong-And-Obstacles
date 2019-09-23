36366b58919c22478559568d71ac0d1be73c7e2e
liamcristello <liamcristello@gmail.com>
2019-09-23 11:05:07 -0400

    Room Scale
    Continued work with scaling the room, progress made.

--- Assets/Scripts/MovePaddle.cs
+++ Assets/Scripts/MovePaddle.cs
@@ -22,8 +22,8 @@ public class MovePaddle : MonoBehaviour
         eastEdge = eastWall.GetComponent<BoxCollider>().center - eastWall.GetComponent<BoxCollider>().size;
         westEdge = westWall.GetComponent<BoxCollider>().center + westWall.GetComponent<BoxCollider>().size;
         //Debug.Log("Bounds are " + westEdge + " and " + eastEdge + ".");
-        leftBound = -15.1f;
-        rightBound = 15.06f;
+        leftBound = -3.45f;
+        rightBound = 3.44f;
     }
 
     // Update is called once per frame
@@ -33,24 +33,24 @@ public class MovePaddle : MonoBehaviour
         //Debug.Log(CoPtoCM(Wii.GetCenterOfBalance(0)));
         //paddleX = Mathf.Clamp(wiiBB.x, transform.TransformPoint(westEdge).x, transform.TransformPoint(eastEdge).x);
         paddleX = Mathf.Clamp(wiiBB.x, leftBound, rightBound);
-        transform.position = new Vector3(paddleX, wiiBB.y);
+        transform.position = new Vector3(paddleX, 22.04f, wiiBB.y);
         // If "Q" is pressed, paddle is large.
         if (Input.GetKeyUp(KeyCode.Q)) {
-            leftBound = -15.1f;
-            rightBound = 15.06f;
-            this.transform.localScale = new Vector3(16.67462f, 0.6537052f, 2.623191f);
+            leftBound = -3.45f;
+            rightBound = 3.44f;
+            this.transform.localScale = new Vector3(5.231771f, 0.1491342f, 0.5984464f);
         }
         // If "W" is pressed, paddle is medium-sized.
         if (Input.GetKeyUp(KeyCode.W)) {
-            leftBound = -17.6f;
-            rightBound = 17.59f;
-            this.transform.localScale = new Vector3(11.67462f, 0.6537052f, 2.623191f);
+            leftBound = -3.57f;
+            rightBound = 3.56f;
+            this.transform.localScale = new Vector3(3.530974f, 0.1491342f, 0.5984464f);
         }
         // If "E" is pressed, paddle is small.
         if (Input.GetKeyUp(KeyCode.E)) {
-            leftBound = -20.08f;
-            rightBound = 20.08f;
-            this.transform.localScale = new Vector3(6.67462f, 0.6537052f, 2.623191f);
+            leftBound = -4.38f;
+            rightBound = 4.39f;
+            this.transform.localScale = new Vector3(1.92535f, 0.1491342f, 0.5984464f);
         }
     }
 
