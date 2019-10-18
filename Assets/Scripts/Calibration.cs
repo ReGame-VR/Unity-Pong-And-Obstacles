ec6df5bb7500ea439adef839811bca0a61196ef2
liamcristello <liamcristello@gmail.com>
2019-10-18 14:51:44 -0400

    Calibration Work

---
+++ Assets/Scripts/Calibration.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Calibration : MonoBehaviour
+{
+    public float leftMax, rightMax;
+    public TextMeshProUGUI leftImg, rightImg, forwardImg, backwardsImg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SaveData()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GlobalControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalControl : MonoBehaviour
+{
+    // floats to store the furthest left and right the participant can lean
+    public float leftMax, rightMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
