8463b37226d8b6e29acf052bb1a1dc8197ac8df0
liamcristello <liamcristello@gmail.com>
2019-10-23 16:52:14 -0400

    Title Scene, UI and Backend Work
    TitleScreen scene added. New UI to select difficulties, working in backend to help implement it.

--- Assets/Scripts/GlobalControl.cs
+++ Assets/Scripts/GlobalControl.cs
@@ -6,15 +6,30 @@ public class GlobalControl : MonoBehaviour
 {
     // floats to store the furthest left and right the participant can lean
     public float leftMax, rightMax;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 
+    public enum ProgressionType { };
+    public enum Difficulty {One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten};
+    public Difficulty difficulty;
 
-    // Update is called once per frame
-    void Update()
+    // participant ID to differentiate logs
+    public string participantID;
+
+    // Single instance of this class
+    public static GlobalControl Instance;
+
+    // Runs on startup
+    private void Awake()
     {
-        
+        // If there is no Instance, makes this the Instance
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        // If an instance already exists, destroy this
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 }
